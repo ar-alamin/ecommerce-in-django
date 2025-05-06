70448e07884a811fe81fc2ae07bd90e67b46a5a7
ar-alamin <aralamin.pi@yahoo.com>
2025-05-06 07:59:18 +0600

    store template add

--- src/core/views.py
+++ src/core/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from apps.store.models import Product
 
 def home(request):
-    return render(request, 'pages/home.html')
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'pages/home.html', context)
