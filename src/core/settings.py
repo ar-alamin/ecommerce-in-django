4c9c95c52d56292c16521fa287271543770f06cf
ar-alamin <aralamin.pi@yahoo.com>
2025-05-08 07:29:21 +0600

    Cart implementation

--- src/core/settings.py
+++ src/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'apps.category',
     'apps.accounts',
     'apps.store',
+    'apps.carts',
 ]
 
 MIDDLEWARE = [
--- src/core/urls.py
+++ src/core/urls.py
@@ -23,5 +23,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('store/', include('apps.store.urls'))
+    path('store/', include('apps.store.urls')),
+    path('cart/', include('apps.carts.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
