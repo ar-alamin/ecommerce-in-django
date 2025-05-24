c64ec6546754a1c80b595cae5623dd8494667cab
ar-alamin <aralamin.pi@yahoo.com>
2025-05-24 07:35:18 +0600

    form customization in resigter page

--- src/core/urls.py
+++ src/core/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('apps.store.urls')),
-    path('cart/', include('apps.carts.urls'))
+    path('cart/', include('apps.carts.urls')),
+    path('accounts/', include('apps.accounts.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
